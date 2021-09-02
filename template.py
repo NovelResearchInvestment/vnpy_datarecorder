4e8facee0940d2cf15077a7a8808e82076299f9d
Gabrielvon <fwlg1113@gmail.com>
2021-09-02 09:11:01 +0800

    Initial commit

---
+++ template.py
@@ -0,0 +1,215 @@
+""""""
+from abc import ABC
+from copy import copy
+from typing import Any, Callable
+
+from vnpy.trader.constant import Interval, Direction, Offset
+from vnpy.trader.object import BarData, TickData, OrderData, TradeData
+from vnpy.trader.utility import virtual
+
+from .base import StopOrder, EngineType
+
+
+class DataTemplate(ABC):
+    """"""
+
+    author = ""
+    parameters = []
+    variables = []
+
+    def __init__(
+        self,
+        cta_engine: Any,
+        strategy_name: str,
+        vt_symbol: str,
+        setting: dict,
+    ):
+        """"""
+        self.cta_engine = cta_engine
+        self.strategy_name = strategy_name
+        self.vt_symbol = vt_symbol
+
+        self.inited = False
+        self.trading = False
+        self.pos = 0
+
+        # Copy a new variables list here to avoid duplicate insert when multiple
+        # strategy instances are created with the same strategy class.
+        self.variables = copy(self.variables)
+        self.variables.insert(0, "inited")
+        self.variables.insert(1, "trading")
+        self.variables.insert(2, "pos")
+
+        self.update_setting(setting)
+
+    def update_setting(self, setting: dict):
+        """
+        Update strategy parameter wtih value in setting dict.
+        """
+        for name in self.parameters:
+            if name in setting:
+                setattr(self, name, setting[name])
+
+    @classmethod
+    def get_class_parameters(cls):
+        """
+        Get default parameters dict of strategy class.
+        """
+        class_parameters = {}
+        for name in cls.parameters:
+            class_parameters[name] = getattr(cls, name)
+        return class_parameters
+
+    def get_parameters(self):
+        """
+        Get strategy parameters dict.
+        """
+        strategy_parameters = {}
+        for name in self.parameters:
+            strategy_parameters[name] = getattr(self, name)
+        return strategy_parameters
+
+    def get_variables(self):
+        """
+        Get strategy variables dict.
+        """
+        strategy_variables = {}
+        for name in self.variables:
+            strategy_variables[name] = getattr(self, name)
+        return strategy_variables
+
+    def get_data(self):
+        """
+        Get strategy data.
+        """
+        strategy_data = {
+            "strategy_name": self.strategy_name,
+            "vt_symbol": self.vt_symbol,
+            "class_name": self.__class__.__name__,
+            "author": self.author,
+            "parameters": self.get_parameters(),
+            "variables": self.get_variables(),
+        }
+        return strategy_data
+
+    @virtual
+    def on_init(self):
+        """
+        Callback when strategy is inited.
+        """
+        pass
+
+    @virtual
+    def on_start(self):
+        """
+        Callback when strategy is started.
+        """
+        pass
+
+    @virtual
+    def on_stop(self):
+        """
+        Callback when strategy is stopped.
+        """
+        pass
+
+    @virtual
+    def on_tick(self, tick: TickData):
+        """
+        Callback of new tick data update.
+        """
+        pass
+
+    @virtual
+    def on_bar(self, bar: BarData):
+        """
+        Callback of new bar data update.
+        """
+        pass
+
+    @virtual
+    def on_trade(self, trade: TradeData):
+        """
+        Callback of new trade data update.
+        """
+        pass
+
+    @virtual
+    def on_order(self, order: OrderData):
+        """
+        Callback of new order data update.
+        """
+        pass
+
+    @virtual
+    def on_stop_order(self, stop_order: StopOrder):
+        """
+        Callback of stop order update.
+        """
+        pass
+
+    def write_log(self, msg: str):
+        """
+        Write a log message.
+        """
+        self.cta_engine.write_log(msg, self)
+
+    def get_engine_type(self):
+        """
+        Return whether the cta_engine is backtesting or live trading.
+        """
+        return self.cta_engine.get_engine_type()
+
+    def get_pricetick(self):
+        """
+        Return pricetick data of trading contract.
+        """
+        return self.cta_engine.get_pricetick(self)
+
+    def load_bar(
+        self,
+        days: int,
+        interval: Interval = Interval.MINUTE,
+        callback: Callable = None,
+        use_database: bool = False
+    ):
+        """
+        Load historical bar data for initializing strategy.
+        """
+        if not callback:
+            callback = self.on_bar
+
+        self.cta_engine.load_bar(
+            self.vt_symbol,
+            days,
+            interval,
+            callback,
+            use_database
+        )
+
+    def load_tick(self, days: int):
+        """
+        Load historical tick data for initializing strategy.
+        """
+        self.cta_engine.load_tick(self.vt_symbol, days, self.on_tick)
+
+    def put_event(self):
+        """
+        Put an strategy data event for ui update.
+        """
+        if self.inited:
+            self.cta_engine.put_strategy_event(self)
+
+    def send_email(self, msg):
+        """
+        Send email to default receiver.
+        """
+        if self.inited:
+            self.cta_engine.send_email(msg, self)
+
+    def sync_data(self):
+        """
+        Sync strategy variables value into disk storage.
+        """
+        if self.trading:
+            self.cta_engine.sync_strategy_data(self)
